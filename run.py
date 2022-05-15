e5616da5ee625e41054919e07b3b55446eab8164
Arron <beale86@msn.com>
2022-05-15 12:35:58 +0000

    Install colorama, email validator, import time, os, random, create colors.py and validation.py

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import gspread
+import time
+import os
+import random
+import validation as val
+from time import sleep
 from google.oauth2.service_account import Credentials
+from email_validator import validate_email, EmailNotValidError
+from colors import Color as Col
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -7,13 +14,14 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('scoreboard')
 
-scores = SHEET.worksheet('scores')
+scores = SHEET.worksheet('scores').get_all_values()
+players = SHEET.worksheet('players').get_all_values()
 
-data = scores.get_all_values()
-
-print(data)
+print(players)
+print(scores)
---
+++ validation.py
@@ -0,0 +1,5 @@
+import gspread
+import time
+from google.oauth2.service_account import Credentials
+from email_validator import validate_email, EmailNotValidError
+from colors import Color as Col
