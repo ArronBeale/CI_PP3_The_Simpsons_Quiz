7e0eb9c78be3116e1c05277245e70724e86d9eb8
Arron <beale86@msn.com>
2022-06-05 13:43:17 +0000

    refactor unit tests to have one assertion per test, replace image in validation folder for README

--- test_validation.py
+++ test_validation.py
@@ -4,27 +4,39 @@ import validation as val
 
 class Test_emails(unittest.TestCase):
     """
-    test validation of emails
+    This will test the validation of the email format
     """
 
     def test_validate_email(self):
         self.assertTrue(val.validate_email('test@test.com'), True)
 
 
-class Test_stats(unittest.TestCase):
+class Test_total_players(unittest.TestCase):
     """
-    Thsi will test the stats functions of total players and
-    total score and total svore average
+    This will test the stats function of total players
     """
 
-    def test_total_player(self):
+    def test_total_players(self):
         self.assertTrue(val.total_players(), True)
+
+
+class Test_total_score(unittest.TestCase):
+    """
+    This will test the stats function of total score
+    """
 
     def test_total_score(self):
         self.assertNotEqual(val.total_score(), 0)
 
+
+class Test_average_score(unittest.TestCase):
+    """
+    This will test the stats function of average score
+    """
+
     def test_average_score(self):
         self.assertNotEqual(val.total_score(), 0)
+
 
 
 if __name__ == '__main__':
